a22676719df931716144b3660332537b0ca41b00
Anatoli Babenia <anatoli@rainforce.org>
2022-01-08 21:04:40 +0300

    Install `curl` for testing web server response

--- 01install.sh
+++ 01install.sh
@@ -7,7 +7,7 @@ set -x
 # enable non-interactive mode to avoid asking questions
 # during installation of Python pip installer
 export DEBIAN_FRONTEND=noninteractive
-apt-get update && apt-get -y install python3-pip
+apt-get update && apt-get -y install python3-pip curl
 
 # install xOpera
 pip3 install opera
